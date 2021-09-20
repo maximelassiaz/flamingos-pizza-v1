8ec7eeeab517e8884dd7eab3a75ed5a68bed3595
Maxime Lassiaz <maxime.lassiaz@gmail.com>
2021-09-20 15:50:54 +0200

    implement header, implement router

--- flamingos-pizza-react-app/src/App.js
+++ flamingos-pizza-react-app/src/App.js
@@ -1,6 +1,29 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 const App = () => {
+  const [isMenuOpen, SetIsMenuOpen] = useState(false)
+
+  const handleClickMenu = () => {
+    SetIsMenuOpen(prev => !prev)
+  }
+
   return (
     <>
+      <Router>
+        <Header 
+          isMenuOpen={isMenuOpen}
+          handleClickMenu={handleClickMenu}
+        />
+        <Switch>
+          <Route exact path="/">
+
+          </Route>
+        </Switch>
+        <Footer /> 
+      </Router>
     </>
   );
 }
---
+++ flamingos-pizza-react-app/src/components/Header.js
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom'
+import { Icon } from '@iconify/react'
+import flamingoIcon from '@iconify/icons-openmoji/flamingo'
+import hamburgerMenu from '@iconify/icons-cil/hamburger-menu'
+
+const Header = ({ isMenuOpen, handleClickMenu }) => {
+    return (
+        <header>
+            <nav id="#navbar" className="navbar">
+                <div className="navbar__brand">
+                    <Icon 
+                        className="navbar__brand-icon"
+                        icon={flamingoIcon}
+                        hFlip={true}
+                    />
+                    <div className="navbar__brand-title brand-title--first-letter">Flamingo's</div>
+                    <div className="navbar__brand-title brand-title--first-letter">Pizza</div>
+                </div>
+                <Icon
+                    className="navbar__hamburger-icon"
+                    icon={hamburgerMenu}
+                    color="#D57A76"
+                    onClick={handleClickMenu}
+                />
+                {isMenuOpen && <ul className="navbar__links">
+                    <li>
+                        <Link 
+                            className="navbar__link"
+                            to="/"
+                            onClick={handleClickMenu}
+                        >
+                            Accueil
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            className="navbar__link"
+                            to="/pizza"
+                            onClick={handleClickMenu}
+                        >
+                            Nos Pizzas
+                        </Link>
+                    </li>
+                    <li>
+                        <Link 
+                            className="navbar__link"
+                            to="/login"
+                            onClick={handleClickMenu}
+                        >
+                            Se connecter
+                        </Link>
+                    </li>
+                </ul>}
+            </nav>
+        </header>
+    )
+}
+
+export default Header
